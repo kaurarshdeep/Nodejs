2e0be2ab5bb77745c0f4abee680ca3af184b88e8
Arshdeep <arshdeepkaurjaggi@gmail.com>
2020-05-17 12:19:50 +0530

    Calculator and many more

---
+++ Firstapp/calculation.js
@@ -0,0 +1,18 @@
+var a = parseInt(process.argv[2]);
+var b = process.argv[3];
+var c = parseInt(process.argv[4]);
+
+if(b =='+'){
+    answer = a + c;
+}
+else if(b =='-'){
+    answer = a - c;
+}
+else if(b =='/'){
+    answer = a / c;
+}
+else if(b =='*'){
+    answer = a * c;
+}
+
+console.log('the answer is: ' +answer);
